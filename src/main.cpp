b9226af650b8286f2f0d0dd9db9a12d808370bcf
transdryad <viswanathsam@gmail.com>
2023-09-28 15:37:44 -0600

    Hello World- from vex

---
+++ src/main.cpp
@@ -0,0 +1,28 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       sam                                                       */
+/*    Created:      9/28/2023, 3:25:07 PM                                     */
+/*    Description:  IQ2 project                                               */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+#include "vex.h"
+
+using namespace vex;
+
+// A global instance of vex::brain used for printing to the IQ2 brain screen
+vex::brain       Brain;
+
+// define your global instances of motors and other devices here
+
+
+int main() {
+	
+    Brain.Screen.printAt( 2, 30, "Hello IQ2" );
+   
+    while(1) {
+        
+        // Allow other tasks to run
+        this_thread::sleep_for(10);
+    }
+}
